1688a19bd3e51dd6b16e6d3dd8533d6d5097970e
Richard Dowden <richard.dowden@purpletonic.com>
2010-12-12 22:20:54 +0000

    changed chef recipe location

--- config/solo.rb
+++ config/solo.rb
@@ -3,7 +3,8 @@
 # 
 # Requires a cookbook path, and a file cache path.
 # 
-cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
+# cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
+cookbook_path File.expand_path(File.join(File.dirname(__FILE__), '..', "cookbooks"))
 log_level         :info
 file_store_path  File.join(File.dirname(__FILE__), '..')
 file_cache_path  File.join(File.dirname(__FILE__), '..')
